859a4e4018f7c585563a54580a9ba18fdff81064
Allen Aby <abyall19@uw.edu>
2022-06-01 14:21:10 -0700

    Updated data downloaders

--- data_downloaders/boolq.sh
+++ data_downloaders/boolq.sh
@@ -1,3 +1,4 @@
 mkdir data/boolq
 cd data/boolq
 wget https://storage.cloud.google.com/boolq/dev.jsonl
+wget https://storage.cloud.google.com/boolq/train.jsonl
--- data_downloaders/obqa.sh
+++ data_downloaders/obqa.sh
@@ -4,3 +4,4 @@ wget https://ai2-public-datasets.s3.amazonaws.com/open-book-qa/OpenBookQA-V1-Sep
 unzip OpenBookQA-V1-Sep2018.zip
 cp OpenBookQA-V1-Sep2018/Data/Main/dev.jsonl dev.jsonl
 cp OpenBookQA-V1-Sep2018/Data/Main/test.jsonl test.jsonl
+cp OpenBookQA-V1-Sep2018/Data/Main/train.jsonl train.jsonl
